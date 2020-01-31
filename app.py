a8f98e5269fd4e25f171a327afeb27e1cf976972
dhananjay-srivastava <srivastavadhananjay16@gmail.com>
2020-01-31 19:19:20 +0530

    nltk downloader

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_pymongo import PyMongo
 
 import nltk
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 stop=stopwords.words('english')
 
